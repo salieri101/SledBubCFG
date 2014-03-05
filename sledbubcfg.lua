b1a2c70aa2940c2058b9f14123ea9ec2461289e8
salieri101 <cruzer@totalcar.hu>
2014-03-05 22:58:09 +0100

    configlua

---
+++ sledbubcfg.lua
@@ -0,0 +1,52 @@
+Server =
+{
+    MaxPlayers                  = 100,
+    BindIP                      = "",
+    BindPort                    = 7777,
+    Timeout                     = 10000,
+    Name                        = " Fun Freeroam. Shop",
+	Description 				= "Sledbub54's Server",
+    Password                    = "",
+    Announce                    = true,
+    SyncUpdate                  = 180
+}
+
+SyncRates =
+{
+    Vehicle                     = 75,
+    OnFoot                      = 120,
+    Passenger                   = 1000,
+    MountedGun                  = 250,
+    StuntPosition               = 350
+}
+
+Streamer =
+{
+	StreamDistance              = 500	
+}
+
+Vehicle =
+{
+    DeathRespawnTime            = 10,
+    DeathRemove                 = false,
+    UnoccupiedRespawnTime       = 45,
+    UnoccupiedRemove            = false,
+}
+
+Player =
+{
+    SpawnPosition               = Vector3( -6550, 209, -3290 )
+}
+
+Module =
+{
+    MaxErrorCount               = 5,
+    ErrorDecrementTime          = 500
+}
+
+World =
+{
+    Time                        = 0.0,
+    TimeStep                    = 0.5,
+    WeatherSeverity             = 1
+}
